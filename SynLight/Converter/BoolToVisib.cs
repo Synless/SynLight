2d6e8f5bc2c33fad8bf6d599cdf5fa612a8e4a39
Synless <Synless@users.noreply.github.com>
2021-04-15 10:32:38 +0200

    Using 'parameter' to flip the logic when needed

--- SynLight/Converter/BoolToVisib.cs
+++ SynLight/Converter/BoolToVisib.cs
@@ -7,14 +7,16 @@ namespace SynLight.Converter
     {
         public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
         {
-            if((bool)value==true)
-            {
-                return System.Windows.Visibility.Visible;
-            }
-            else
-            {
-                return System.Windows.Visibility.Collapsed;
-            }
+            bool toCompare = true;
+
+            if(parameter != null)            
+                if(bool.TryParse(parameter.ToString(), out bool b_parameter))                
+                    toCompare = b_parameter;
+                
+            if(bool.TryParse(value.ToString(), out bool _value))
+                return _value == toCompare ? System.Windows.Visibility.Visible : System.Windows.Visibility.Collapsed;
+
+            return System.Windows.Visibility.Visible;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
