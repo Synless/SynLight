b5b54065995fb3f719a4504096910c4d42dfbc1b
Synless <Synless@users.noreply.github.com>
2023-10-20 16:51:23 +0200

    Add files via upload

--- Arduino/SynLight_Serial/SynLight_Serial.ino
+++ Arduino/SynLight_Serial/SynLight_Serial.ino
@@ -2,7 +2,7 @@
 #define NUM_LEDS 112
 #define DATA_PIN 2
 
-const int maxBrightness = 80;
+const int maxBrightness = 90;
 
 CRGB leds[NUM_LEDS];
 byte buffer[NUM_LEDS*3];
@@ -11,34 +11,20 @@ int red = 0;
 int green = 0;
 int blue = 0;
 
-
-void fill(int _start, int _end, int r, int g, int b)
-{
-    for (int n = _start; n < NUM_LEDS && n < _end; n++) { leds[n] = CRGB(r,g,(b*3)>>2); }
-    FastLED.show(); 
-}
-
 void setup()
 {
     Serial.begin(115200);
     FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
     setBrightness(maxBrightness);
-    fill(0,NUM_LEDS,0,100,0);
-    delay(250);
-    fill(0,NUM_LEDS,0,0,0);
-    delay(250);
-    fill(0,NUM_LEDS,0,100,0);
-    delay(250);
-    fill(0,NUM_LEDS,0,0,0);
 }
 
-long i;
 long t1 = 0;
 long t2 = 0;
 long t3 = 0;
 int br = 0;
 bool found;
 bool start_fade = false;
+bool serial_found = false;
 
 void loop()
 {
@@ -46,7 +32,10 @@ void loop()
 
     Serial.readBytes(buffer, sizeof(buffer));
     Serial.read();
-
+    
+    if(!serial_found && buffer[0] == 'p' && buffer[1] == 'i' && buffer[2] == 'n' && buffer[3] == 'g')        
+        Serial.println("pong");
+            
     found = false;
 
     for(int n = 0; n < NUM_LEDS; n++)
@@ -59,9 +48,8 @@ void loop()
         buffer[n*3] = 0;
         buffer[(n*3) + 1] = 0;
         buffer[(n*3) + 2] = 0;
-
-        i = red+green+blue;
-        if(i!=0)
+        
+        if(red+green+blue != 0)
         {
             found = true;
             start_fade = false;
@@ -71,26 +59,19 @@ void loop()
     
     if(t1 - t2 > 500)
     {
-        Serial.println("500");
-        t2 = millis();
-        if(!found)
-        {
-            start_fade = true;
-        }
+        t2 = millis();        
+        if(!found) { start_fade = true; }
     }
     if(t1 - t3 > 200)
     {
-        Serial.println("200");
         t3 = millis();
         if(found)
         {
-            Serial.println("found");
             br = max(0,min(maxBrightness,br+12));
             setBrightness(br);
         }
         else if(start_fade)
         {
-            Serial.println("start_fade");
             br = max(0,min(maxBrightness,br-10));
             setBrightness(br);
         }
@@ -99,11 +80,10 @@ void loop()
     FastLED.show();
 }
 
-void setBrightness(int brightness)
+void fill(int _start, int _end, int r, int g, int b)
 {
-    FastLED.setBrightness(max(0,min(maxBrightness,brightness)));
-    Serial.print("brightness : ");
-    Serial.println(brightness);
-    Serial.print("caped_brightness : ");
-    Serial.println(max(0,min(maxBrightness,brightness)));
+    for (int n = _start; n < NUM_LEDS && n < _end; n++) { leds[n] = CRGB(r,g,(b*3)>>2); }
+    FastLED.show(); 
 }
+
+void setBrightness(int brightness) { FastLED.setBrightness(max(0,min(maxBrightness,brightness))); }
